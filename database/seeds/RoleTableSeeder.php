14a4a42479540959b1862d93031d96d88872d029
Tarika Panjwani <tarika@savitriya.com>
2017-10-05 17:07:54 +0530

    added new roles

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -14,9 +14,19 @@ class RoleTableSeeder extends Seeder
     {
         $roles = [
             [
+                'name' => 'Director',
+                'display_name' => 'Director',
+                'description' => 'This profile will have all the permissions'
+            ],
+            [
                 'name' => 'Administrator',
                 'display_name' => 'Administrator',
                 'description' => 'This profile will have all the permissions'
+            ],
+            [
+                'name' => 'Manager',
+                'display_name' => 'Manager',
+                'description' => 'This profile will have permissions of job open , candidate,client , interview  modules. No administrative rights'
             ],
             [
                 'name' => 'Senior Lead Consultant',
