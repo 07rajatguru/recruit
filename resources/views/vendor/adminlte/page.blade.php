8849f29b0e4419cf53d2e73a72b22258545b2ceb
Tarika Panjwani <tarika@savitriya.com>
2018-02-08 17:20:37 +0530

    enable modules

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -57,7 +57,7 @@
                                 <div class="navbar-custom-menu">
 
                                     <ul class="nav navbar-nav">
-                                        {{--<li class="dropdown messages-menu">
+                                        <li class="dropdown messages-menu">
                                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                                 <i class="fa fa-plus"></i>
                                                 ADD
@@ -66,7 +66,7 @@
                                                 <li>
                                                     <!-- inner menu: contains the actual data -->
                                                     <div class="slimScrollDiv" style="position: relative; overflow: hidden; width: auto; height: 200px;">
-                                                        <ul class="menu" style="overflow: hidden; width: 100%; height: 200px;">
+                                                        <ul class="menu" style="overflow: hidden; width: 100%; height;150px">
                                                             <li>
                                                                 <a href="/jobs/create">
                                                                     Add Job Openings
@@ -82,7 +82,7 @@
                                                                     Add Candidate
                                                                 </a>
                                                             </li>
-                                                            <li>
+                                                            {{--<li>
                                                                 <a href="/interview/create">
                                                                     Add Interview
                                                                 </a>
@@ -91,11 +91,11 @@
                                                                 <a href="/todos/create">
                                                                     Add Todos
                                                                 </a>
-                                                            </li>
+                                                            </li>--}}
                                                         </ul><div class="slimScrollBar" style="background-color: rgb(0, 0, 0); width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 1px; height: 131.14754098360655px; background-position: initial initial; background-repeat: initial initial;"></div><div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; background-color: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px; background-position: initial initial; background-repeat: initial initial;"></div></div>
                                                 </li>
                                             </ul>
-                                        </li>--}}
+                                        </li>
 
 
                                         {{--<li class="dropdown messages-menu">
