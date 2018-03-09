d4710554d01a4c7d4b7a04519faeff33a4b26b87
Tarika Panjwani <tarika@savitriya.com>
2018-03-09 16:54:04 +0530

    Changes in job open module

--- app/Http/Controllers/JobOpenController.php
+++ app/Http/Controllers/JobOpenController.php
@@ -227,7 +227,8 @@ class JobOpenController extends Controller
 
         $action = "add";
 
-        $selected_users = array($user_id);
+        $super_admin_user_id = getenv('SUPERADMINUSERID');
+        $selected_users = array($user_id,$super_admin_user_id);
         return view('adminlte::jobopen.create', compact('user_id','action', 'industry', 'client', 'users', 'job_open_status', 'job_type','job_priorities','selected_users'));
 
     }
