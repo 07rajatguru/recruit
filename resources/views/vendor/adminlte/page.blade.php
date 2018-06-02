6e605ebb291d332c7fdcf583130fbe766b4782f5
Tarika Panjwani <tarika@savitriya.com>
2018-06-02 12:02:35 +0530

    scroll bar

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -113,7 +113,7 @@
                                                 <li>
                                                     <!-- inner menu: contains the actual data -->
                                                     <div class="slimScrollDiv " style="position: relative; overflow: hidden; width: auto; height: 200px;">
-                                                        <ul class="menu notification-ul" style="overflow: hidden; width: 100%; height: 200px;">
+                                                        <ul class="menu notification-ul" style="overflow: hidden; width: 100%; height: 200px;overflow-y: scroll !important;">
                                                         </ul>
                                                         <div class="slimScrollBar" style="background-color: rgb(0, 0, 0); width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 1px; height: 131.14754098360655px; background-position: initial initial; background-repeat: initial initial;"></div>
                                                         <div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; background-color: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px; background-position: initial initial; background-repeat: initial initial;"></div></div>
@@ -132,7 +132,7 @@
                                                 <li>
                                                     <!-- inner menu: contains the actual data -->
                                                     <div class="slimScrollDiv " style="position: relative; overflow: hidden; width: auto; height: 200px;">
-                                                        <ul class="menu todos-ul" style="overflow: hidden; width: 100%; height: 200px;">
+                                                        <ul class="menu todos-ul" style="overflow: hidden; width: 100%; height: 200px;overflow-y: scroll !important;">
                                                         </ul>
                                                         <div class="slimScrollBar" style="background-color: rgb(0, 0, 0); width: 3px; position: absolute; top: 0px; opacity: 0.4; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; z-index: 99; right: 1px; height: 131.14754098360655px; background-position: initial initial; background-repeat: initial initial;"></div>
                                                         <div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-top-left-radius: 7px; border-top-right-radius: 7px; border-bottom-right-radius: 7px; border-bottom-left-radius: 7px; background-color: rgb(51, 51, 51); opacity: 0.2; z-index: 90; right: 1px; background-position: initial initial; background-repeat: initial initial;"></div></div>
