c370083666a491de4f80f3f8f0e5a17c60a3d0c8
Tarika Panjwani <tarika@savitriya.com>
2018-06-29 12:41:03 +0530

    daily report css

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -98,10 +98,10 @@ class ReportController extends Controller
 
             \Mail::send('adminlte::emails.dailyReport', $input, function ($message) use($input) {
                 $message->from($input['from_address'], $input['from_name']);
-                $message->to($input['to'])->cc($input['cc'])->subject('Activity Report - '.$input['value'] . ' - ' . date("d-m-Y"));
+                $message->to($input['to'])->cc($input['cc'])->subject('Daily Report - '.$input['value'] . ' - ' . date("d-m-Y"));
             });
 
-           //return view('adminlte::emails.DailyReport', compact('associate_daily','associate_count','lead_count','interview_daily','interview_count','users'));
+           //return view('adminlte::emails.DailyReport', compact('app_url','associate_daily','associate_count','lead_count','interview_daily','interview_count','users'));
         }
     }
 
