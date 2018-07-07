6017426cbcc259cce5ed5c89e84a464a27eb85ca
Tarika Panjwani <tarika@savitriya.com>
2018-07-07 11:24:53 +0530

    added icons on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,20 +30,21 @@
 
 
     <div class="row">
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-aqua">
+
+        <div class="col-lg-2 col-xs-4">
+            <div class="small-box bg-red">
                 <div class="inner">
-                    <h3> {{ $interviewCount or 0}} </h3>
-                    <p>Today's and Tomorrow's Interviews</p>
+                    <h3>{{ $clientCount or 0 }}</h3>
+                    <p>Clients added this month</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-bag"></i>
+                    <i class="ion ion-person-add"></i>
                 </div>
-                <a href="interview" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="client" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-lg-2 col-xs-4">
             <div class="small-box bg-green">
                 <div class="inner">
                     <h3>{{ $jobCount or 0 }}</h3>
@@ -56,7 +57,33 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-xs-6">
+        <div class="col-lg-2 col-xs-4">
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>No. of CVS associated this month</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-pie-graph"></i>
+                </div>
+                <a href="jobs" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-xs-4">
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3> {{ $interviewCount or 0}} </h3>
+                    <p>Today's and Tomorrow's Interviews</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="interview" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <div class="col-lg-2 col-xs-4">
             <div class="small-box bg-yellow">
                 <div class="inner">
                     <h3> {{$candidatejoinCount}}</h3>
@@ -69,18 +96,19 @@
             </div>
         </div>
 
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-red">
+        <div class="col-lg-2 col-xs-4">
+            <div class="small-box bg-yellow">
                 <div class="inner">
-                    <h3>{{ $clientCount or 0 }}</h3>
-                    <p>Clients added this month</p>
+                    <h3> 0 </h3>
+                    <p>No. of interviews attended this month</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-person-add"></i>
+                    <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="client" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="candidatejoin" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
+
     </div>
 
     <div class="row">
