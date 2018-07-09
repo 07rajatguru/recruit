81564f6571a540bd0ed626935428c277bd1486d4
Shahid <shahid@trajinfotech.com>
2018-07-09 15:42:16 +0530

    todaytomorrow link comment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -79,7 +79,7 @@
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="todaytomorrow" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
