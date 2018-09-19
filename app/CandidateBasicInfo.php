d4d5e8f0da98b56247a1397f6faae24cc7921368
Shahid <shahid@trajinfotech.com>
2018-09-19 12:12:03 +0530

    Candidate > change query

--- app/CandidateBasicInfo.php
+++ app/CandidateBasicInfo.php
@@ -69,7 +69,10 @@ class CandidateBasicInfo extends Model
             $query = $query->offset($offset);
         }
         if (isset($search) && $search != '') {
-            $query = $query->where('full_name','like','%$search%');
+            $query = $query->where('candidate_basicinfo.full_name','like',"%$search%");
+            $query = $query->orwhere('users.name','like',"%$search%");
+            $query = $query->orwhere('candidate_basicinfo.email','like',"%$search%");
+            $query = $query->orwhere('candidate_basicinfo.mobile','like',"%$search%");
         }
         $res = $query->get();
 
@@ -94,7 +97,10 @@ class CandidateBasicInfo extends Model
         $query = $query->leftjoin('users','users.id','=','candidate_otherinfo.owner_id');
         $query = $query->select('candidate_basicinfo.id as id', 'candidate_basicinfo.full_name as fname', 'candidate_basicinfo.lname as lname','candidate_basicinfo.email as email', 'users.name as owner', 'candidate_basicinfo.mobile as mobile');
         if (isset($search) && $search != '') {
-            $query = $query->where('full_name','like','%$search%');
+            $query = $query->where('candidate_basicinfo.full_name','like',"%$search%");
+            $query = $query->orwhere('users.name','like',"%$search%");
+            $query = $query->orwhere('candidate_basicinfo.email','like',"%$search%");
+            $query = $query->orwhere('candidate_basicinfo.mobile','like',"%$search%");
         }
         $res = $query->count();
 
