901183a04896a0558eb796a9bc2b80721966cff3
Tarika Panjwani <tarika@savitriya.com>
2018-10-03 18:30:21 +0530

    tets mail

---
+++ resources/views/vendor/adminlte/emails/sample.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Adler Talent</title>
+
+    @yield('style')
+</head>
+
+<body>
+<table border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Helvetica,Arial,sans-serif; border-collapse: collapse; color: #444444;" align="center">
+
+    <tr>
+        <td width="600" >
+            <table  cellpadding="0" cellspacing="0" style="border:0; background-color: #ffffff; padding: 50px 54px;">
+                <tr>
+                    <td>
+                        <b><p style="margin-top: 0px; margin-bottom: 14px; font-family: arial;">Hello, </p></b>
+
+                        <p>Thanks & Regards,</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr width="600" style="height: 45px; background-color: #dddddd;">
+        <td style="text-align: center; font-size: 11px; color: #888888; font-family: arial;">Copyright Adler Talent <?php echo date('Y'); ?>. All rights reserved</td>
+    </tr>
+</table>
+</body>
+</html>
