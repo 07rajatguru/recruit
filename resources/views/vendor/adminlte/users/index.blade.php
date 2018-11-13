938a039c13400d00680dee7ae4b7c46aa16f4dff
Shahid <shahid@trajinfotech.com>
2018-11-13 17:28:00 +0530

    Users > Delete Pop-up

--- resources/views/vendor/adminlte/users/index.blade.php
+++ resources/views/vendor/adminlte/users/index.blade.php
@@ -55,9 +55,10 @@
                 <td>
                     <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
                     <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-                    {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                    {{--{!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
                     {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                    {!! Form::close() !!}
+                    {!! Form::close() !!}--}}
+                    @include('adminlte::partials.deleteModalUser', ['data' => $user, 'name' => 'users','display_name'=>'User'])
                 </td>
 
             </tr>
