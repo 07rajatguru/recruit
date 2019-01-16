37f6148e6e6904084b611fc16d6c9ea75d5cf0ab
Shahid <shahid@trajinfotech.com>
2019-01-16 19:01:13 +0530

    working on client convert script

--- app/Console/Commands/ClientStatus.php
+++ app/Console/Commands/ClientStatus.php
@@ -48,53 +48,33 @@ class ClientStatus extends Command
         ->leftjoin('job_openings','client_basicinfo.id','=','job_openings.client_id')
         ->leftjoin('job_associate_candidates','job_associate_candidates.job_id','=','job_openings.id')
         ->select('job_openings.*','client_basicinfo.id as Client_Id','job_associate_candidates.created_at as created')
-        ->orderBy('job_openings.created_at','desc')
         ->groupBy('client_basicinfo.id')
+        ->orderBy('job_openings.created_at','desc')
+        ->where('client_basicinfo.id','=','30')
         ->get();
-
-        /*$client_query = ClientBasicinfo::query();
-        $client_query = $client_query->leftJoin('job_openings','job_openings.client_id','=','client_basicinfo.id');
-        $client_query = $client_query->leftJoin('job_associate_candidates','job_associate_candidates.job_id','=','job_openings.id');
-        $client_query = $client_query->select('job_openings.*','client_basicinfo.id as Client_Id','job_associate_candidates.created_at as created');
-        $job_open = $client_query->get();*/
-
+        print_r($job_open);exit;
         $job=array();
+        if(isset($job_open)){
+            foreach($job_open as $key=>$value){
 
-        if(isset($job_open))
-        {
-            foreach($job_open as $key=>$value)
-            {
                 $client_id=$value->Client_Id;
 
                 $created_at = $value->created_at; // job added date
-                $created = $value->created; // job candidate associate date
+                //$created = $value->created; // job candidate associate date
 
                 $date1=date('Y-m-d',strtotime("-30 days"));
-
-                if(isset($created_at) )
-                {
-                   
-                   if(($created_at || $created) < $date1)
-                   {
-                        DB::statement("UPDATE  client_basicinfo SET `status`='0' WHERE `id`='$client_id'");
-
-                        //echo "Inacitve";
+                if(isset($created_at) ){
+                   if(($created_at/* || $created*/) < $date1){
+                        DB::statement("UPDATE client_basicinfo SET `status`='0' WHERE `id`='$client_id'");
                    }
-                   else
-                   {
-                        DB::statement("UPDATE  client_basicinfo SET `status`='1' WHERE `id`='$client_id'");
-
-                       // echo "Active";
+                   else{
+                        DB::statement("UPDATE client_basicinfo SET `status`='1' WHERE `id`='$client_id'");
                    }
                 }
                 else{
-
-                    DB::statement("UPDATE  client_basicinfo SET `status`='0' WHERE `id`='$client_id'");
-
-                   // echo "Inacitve";
+                    DB::statement("UPDATE client_basicinfo SET `status`='0' WHERE `id`='$client_id'");
                 }
             }
         }
-
     }
 }
