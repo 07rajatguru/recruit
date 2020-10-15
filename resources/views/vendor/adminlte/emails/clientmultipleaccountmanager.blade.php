db88db9e0eace43138b10e779f6f2484de02be93
Dhara Khandhediya <dhara@trajinfotech.com>
2020-10-15 12:08:22 +0530

    Email format changes

--- resources/views/vendor/adminlte/emails/clientmultipleaccountmanager.blade.php
+++ resources/views/vendor/adminlte/emails/clientmultipleaccountmanager.blade.php
@@ -8,48 +8,53 @@
 </head>
 
 <body>
-    <table border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Cambria; border-collapse: collapse; color: #444444;padding: 50px 54px;border:0" align="center">
+    <table border="0" cellpadding="0" cellspacing="0" width="600" style="font-family:Helvetica,Arial,sans-serif; border-collapse: collapse; color: #444444;" align="center">
         <tr>
             <td width="600">
-                <b><p>Dear Team,</p></b>
-                <i><p>Greetings !</p></i>
-                <p><u>Please find transferred client details as below :</u></p>
-                <p>
-                <table width="580" cellpadding="3" cellspacing="0" border="1" border-color="#000000">
-                    <tr style="background-color: #7598d9">
-                        <td align="center" style="font-family: Cambria;"><b>Sr.No.</b></td>
-                        <td align="center" style="font-family: Cambria;"><b>Company Name</b></td>
-                        <td align="center" style="font-family: Cambria;"><b>Contact Point</b></td>
-                        <td align="center" style="font-family: Cambria;"><b>Location</b></td>
-                        <td align="center" style="font-family: Cambria;"><b>Transferred From</b></td>
-                        <td align="center" style="font-family: Cambria;"><b>Transferred to</b></td>
+                <table width="100%" cellpadding="0" cellspacing="0" style="border:0; background-color: #ffffff; padding: 50px 54px;">
+                    <tr style="font-family:Cambria, serif;font-size: 11.0pt;">
+                        <td style="font-family:Cambria, serif;font-size: 11.0pt;">
+                            <b><p style="margin-top: 0px; margin-bottom: 14px;">Dear Team,</p></b>
+                            <i><p>Greetings !</p></i>
+                            <p><u>Please find transferred client details as below :</u></p>
+                            <table width="580" cellpadding="3" cellspacing="0" border="1" border-color="#000000">
+                                <tr style="background-color: #7598d9">
+                                    <td align="center" style="font-family: Cambria;"><b>Sr.No.</b></td>
+                                    <td align="center" style="font-family: Cambria;"><b>Company Name
+                                    </b></td>
+                                    <td align="center" style="font-family: Cambria;"><b>Contact Point
+                                    </b></td>
+                                    <td align="center" style="font-family: Cambria;"><b>Location</b>
+                                    </td>
+                                    <td align="center" style="font-family: Cambria;"><b>Transferred From</b></td>
+                                    <td align="center" style="font-family: Cambria;"><b>Transferred to</b></td>
+                                </tr>
+                                <?php $i=0; ?>
+                                @foreach($client_info as $key => $value)
+                                    <tr>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ ++$i }}</td>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ $value['name'] }}</td>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ $value['coordinator_name'] }}</td>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ $value['billing_city'] }}</td>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ $value['transferred_from'] }}</td>
+                                        <td align="center" style="font-family: Cambria;">
+                                            {{ $value['transferred_to'] }}</td>
+                                    </tr>
+                                @endforeach
+                            </table>
+
+                            <p>For any query / discussion, feel free to connect with Client Servicing team via email.</p>
+                            <p>Thanks.</p>
+                            <p>Best Wishes,</p>
+                            <p>Easy2Hire Team</p>
+                        </td>
                     </tr>
-                    <?php $i=0; ?>
-                    @foreach($client_info as $key => $value)
-                        <tr>
-                            <td align="center" style="font-family: Cambria;">{{ ++$i }}</td>
-                            <td align="center" style="font-family: Cambria;">{{ $value['name'] }}</td>
-                            <td align="center" style="font-family: Cambria;">{{ $value['coordinator_name'] }}
-                            </td>
-                            <td align="center" style="font-family: Cambria;">{{ $value['billing_city'] }}</td>
-                            <td align="center" style="font-family: Cambria;">{{ $value['transferred_from'] }}
-                            </td>
-                            <td align="center" style="font-family: Cambria;">{{ $value['transferred_to'] }}
-                            </td>
-                        </tr>
-                    @endforeach
-                    <table width="580" cellpadding="3" cellspacing="0" border="0">
-                        <tr style="font-family:Cambria, serif;font-size: 11.0pt;">
-                            <td style="font-family:Cambria, serif;font-size: 11.0pt;">
-                                <p>For any query / discussion, feel free to connect with Client Servicing team via email.</p>
-                                <p>Thanks.</p>
-                                <p>Best Wishes,</p>
-                                <p>Easy2Hire Team</p>
-                            </td>
-                        </tr>
-                    </table>
                 </table>
-                </p>
             </td>
         </tr>
     </table>
