2c7897e167bcd1fe30afff95ea6e980cc599675f
Dhara Khandhediya <dhara@trajinfotech.com>
2020-12-25 09:47:36 +0530

    Lead convert to client display in client list

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -627,6 +627,7 @@ class LeadController extends Controller
          
         $client_basic_info->created_at = time();
         $client_basic_info->updated_at = time();
+        $client_basic_info->delete_client = 0;
 
         if($client_basic_info->save()) {
 
