cc10183e8d5d70e57f385d91ec31b9b46e8056b4
Dhara Khandhediya <dhara@trajinfotech.com>
2021-01-12 16:01:58 +0530

    Permissions view changes

--- resources/views/vendor/adminlte/new_role/view.blade.php
+++ resources/views/vendor/adminlte/new_role/view.blade.php
@@ -1,3 +1,13 @@
+@section('customs_css')
+    <style>
+        div.scrollmenu {
+            background-color: white;
+            overflow: auto;
+            white-space: nowrap;
+        }
+    </style>
+@endsection
+
 @extends('adminlte::page')
 
 @section('title', 'Rolewise Permissions')
@@ -7,41 +17,43 @@
 @stop
 
 @section('content')
-	<table class="table table-striped table-bordered nowrap" cellspacing="0" width="100%" id="roles_table">
-        <thead>
-	        <tr>
-	            <th>Permission</th>
+    <div class="scrollmenu">
+    	<table class="table table-striped table-bordered nowrap" cellspacing="0" width="100%" id="roles_table" style="border-color: black;">
+            <thead>
+    	        <tr>
+    	            <th style="white-space: pre-wrap; word-wrap: break-word; color:black; text-decoration:none;background-color: #DCDCDC;border-color: black;">Permission</th>
 
-	            @if(isset($roles) && sizeof($roles) > 0)
-		            @foreach($roles as $key => $value)
-		                <th><center><b>{{ $value['name'] }}</b></center></th>
-		            @endforeach
-		        @endif
-	        </tr>
-    	</thead>
-        <tbody>
-	        @if(isset($permissions) && sizeof($permissions) > 0)
-	            @foreach($permissions as $key => $value)
-	                <tr id="{{ $value['id'] }}" class="permission_id">
-	                    <td>{{ $value['display_name'] }}</td>
+    	            @if(isset($roles) && sizeof($roles) > 0)
+    		            @foreach($roles as $key => $value)
+    		                <th style="white-space: pre-wrap; word-wrap: break-word; color:black; text-decoration:none;background-color: #DCDCDC;border-color: black;"><center><b>{{ $value['display_name'] }}</b></center></th>
+    		            @endforeach
+    		        @endif
+    	        </tr>
+        	</thead>
+            <tbody>
+    	        @if(isset($permissions) && sizeof($permissions) > 0)
+    	            @foreach($permissions as $key => $value)
+    	                <tr id="{{ $value['id'] }}" class="permission_id" style="border-color: black;">
+    	                   <td style="white-space: pre-wrap; word-wrap: break-word; color:black; text-decoration:none;">{{ $value['display_name'] }}</td>
 
-	                    @if(isset($roleswise) && sizeof($roleswise) > 0)
-	                    	@foreach($roleswise as $k => $v)
-                                <?php 
-                                    $v_array = explode(",", $v);
-                                ?>
-                                @if(in_array($value['id'], $v_array))
-				                    <td align="center"><input type="checkbox" class="rolecb" id="{{ $k }}" checked=""></td>
-                                @else
-                                    <td align="center"><input type="checkbox" class="rolecb" id="{{ $k }}"></td>
-                                @endif
-				            @endforeach
-	                    @endif
-	                </tr>
-	            @endforeach
-	        @endif
-        </tbody>
-    </table>
+        	                @if(isset($roleswise) && sizeof($roleswise) > 0)
+        	                    @foreach($roleswise as $k => $v)
+                                    <?php 
+                                        $v_array = explode(",", $v);
+                                    ?>
+                                    @if(in_array($value['id'], $v_array))
+        				                <td align="center"><input type="checkbox" class="rolecb" id="{{ $k }}" checked=""></td>
+                                    @else
+                                        <td align="center"><input type="checkbox" class="rolecb" id="{{ $k }}"></td>
+                                    @endif
+        				        @endforeach
+        	                @endif
+    	                </tr>
+    	            @endforeach
+    	        @endif
+            </tbody>
+        </table>
+    </div>
 @stop
 
 @section('customscripts')
