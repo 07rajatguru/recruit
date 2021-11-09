c55764337ee99d54833fa2a32cbacd9a0bde1c1b
Dhara Khandhediya <dhara@trajinfotech.com>
2021-11-09 16:30:25 +0530

    do changes in interview scheduled email fromat

--- resources/views/vendor/adminlte/emails/interviewmultipleschedule.blade.php
+++ resources/views/vendor/adminlte/emails/interviewmultipleschedule.blade.php
@@ -15,60 +15,60 @@
                 <p style="text-align: left;"><u>Please find interview schedule as below for your kind reference:</u></p>
                     <table width="600" cellpadding="3" cellspacing="0" border="1" border-color="#000000">
                         <tr style="background-color: #7598d9;font-family:Cambria, serif;font-size: 11.0pt;">
-                            <td align="left"><b>Sr. No.</b></td>
-                            <td align="left"><b>Position</b></td>
-                            <td align="left"><b>Position Location</b></td>
-                            <td align="left"><b>Name of the Candidate</b></td>
-                            <td align="left"><b>Interview Date</b></td>
-                            <td align="left"><b>Interview Time</b></td>
-                            <td align="left"><b>Interview Location</b></td>
-                            <td align="left"><b>Candidate Location</b></td>
-                            <td align="left"><b>Mode of Interview</b></td>
+                            <td align="center"><b>Sr. No.</b></td>
+                            <td align="center"><b>Position</b></td>
+                            <td align="center"><b>Position Location</b></td>
+                            <td align="center"><b>Name of the Candidate</b></td>
+                            <td align="center"><b>Interview Date</b></td>
+                            <td align="center"><b>Interview Time</b></td>
+                            <td align="center"><b>Interview Location</b></td>
+                            <td align="center"><b>Candidate Location</b></td>
+                            <td align="center"><b>Mode of Interview</b></td>
 
                             @if(isset($type_string) && $type_string != '')
                                 @if(strpos($type_string,"Video") !== false)
-                                    <td align="left"><b>Video ID</b></td>
+                                    <td align="center"><b>Video ID</b></td>
                                 @endif
                             @endif
 
-                            <td align="left"><b>Contact No.</b></td>
-                            <td align="left"><b>Email ID</b></td>
-                            <td align="left"><b>Confirmed</b></td>
-                            <td align="left"><b>Source</b></td>
+                            <td align="center"><b>Contact No.</b></td>
+                            <td align="center"><b>Email ID</b></td>
+                            <td align="center"><b>Confirmed</b></td>
+                            <td align="center"><b>Source</b></td>
 
                             @if(isset($yesterday_date) && $yesterday_date != '')
-                                <td align="left"><b>Action</b></td>
+                                <td align="center"><b>Action</b></td>
                             @endif
                         </tr>
 
                         <?php $i=0; ?>
                         @foreach($interview_details as $key => $value)
                             <tr style="font-family:Cambria, serif;font-size: 11.0pt;">
-                                <td align="left">{{ ++$i }}</td>
-                                <!-- <td align="left">{{$value['client_name']}} - {{$value['job_designation']}}</td> -->
+                                <td align="center">{{ ++$i }}</td>
+                                <!-- <td align="center">{{$value['client_name']}} - {{$value['job_designation']}}</td> -->
 
-                                <td align="left">{{ $value['job_designation'] }}</td>
-                                <td align="left">{{$value['job_location']}}</td>
-                                <td align="left">{{$value['cname']}}</td>
-                                <td align="left">{{date('d/m/Y',strtotime($value['interview_date'])) }}</td>
-                                <td align="left">{{date('h:i A',strtotime($value['interview_time']))  }}</td>
-                                <td align="left">{{$value['interview_location']}}</td>
-                                <td align="left">{{$value['candidate_location']}}</td>
-                                <td align="left">{{$value['interview_type']}}</td>
+                                <td align="center">{{ $value['job_designation'] }}</td>
+                                <td align="center">{{$value['job_location']}}</td>
+                                <td align="center">{{$value['cname']}}</td>
+                                <td align="center">{{date('d/m/Y',strtotime($value['interview_date'])) }}</td>
+                                <td align="center">{{date('h:i A',strtotime($value['interview_time']))  }}</td>
+                                <td align="center">{{$value['interview_location']}}</td>
+                                <td align="center">{{$value['candidate_location']}}</td>
+                                <td align="center">{{$value['interview_type']}}</td>
 
                                 @if(isset($type_string) && $type_string != '')
                                     @if(strpos($type_string,"Video") !== false)
-                                        <td align="left">{{$value['skype_id']}}</td>
+                                        <td align="center">{{$value['skype_id']}}</td>
                                     @endif
                                 @endif
-                                
-                                <td align="left">{{$value['cmobile']}}</td>
-                                <td align="left">{{$value['cemail']}}</td>
-                                <td align="left"><b>{{'Yes'}}</b></td>
-                                <td align="left"><b>{{'Adler'}}</b></td>
+
+                                <td align="center">{{$value['cmobile']}}</td>
+                                <td align="center">{{$value['cemail']}}</td>
+                                <td align="center"><b>{{'Yes'}}</b></td>
+                                <td align="center"><b>{{'Adler'}}</b></td>
 
                                 @if(isset($yesterday_date) && $yesterday_date != '')
-                                    <td align="left">
+                                    <td align="center">
                                         <a formtarget="_blank" href="{{getenv('APP_URL').'/interview/'.$value['id'].'/edit/index'}}">Edit</a>
                                     </td>
                                 @endif
--- resources/views/vendor/adminlte/emails/interviewschedule.blade.php
+++ resources/views/vendor/adminlte/emails/interviewschedule.blade.php
@@ -15,57 +15,65 @@
                 <p style="text-align: left;"><u>Please find interview schedule as below for your kind reference:</u></p>
                     <table width="580" cellpadding="3" cellspacing="0" border="1" border-color="#000000">
                         <tr style="background-color: #7598d9;font-family:Cambria, serif;font-size: 11.0pt;">
-                            <td align="left"><b>Sr. No.</b></td>
-                            <td align="left"><b>Position</b></td>
-                            <td align="left"><b>Position Location</b></td>
-                            <td align="left"><b>Name of the Candidate</b></td>
-                            <td align="left"><b>Interview Date</b></td>
-                            <td align="left"><b>Interview Time</b></td>
+                            <td align="center"><b>Sr. No.</b></td>
+                            <td align="center"><b>Position</b></td>
+                            <td align="center"><b>Position Location</b></td>
+                            <td align="center"><b>Name of the Candidate</b></td>
+                            <td align="center"><b>Interview Date</b></td>
+                            <td align="center"><b>Interview Time</b></td>
 
                             @if(isset($interview_type) && $interview_type == 'Personal Interview')
-                                <td align="left"><b>Interview Location</b></td>
+                                <td align="center"><b>Interview Location</b></td>
                             @endif
                             
-                            <td align="left"><b>Candidate Location</b></td>
-                            <td align="left"><b>Mode of Interview</b></td>
+                            <td align="center"><b>Candidate Location</b></td>
+                            <td align="center"><b>Mode of Interview</b></td>
 
-                            @if((isset($interview_type) && $interview_type == 'Personal Interview') || (isset($interview_type) && $interview_type == 'Telephonic Interview'))
+                            {{-- @if((isset($interview_type) && $interview_type == 'Personal Interview') || (isset($interview_type) && $interview_type == 'Telephonic Interview'))
 
                             @else
-                                <td align="left"><b>Video ID</b></td>
-                            @endif
+                                <td align="center"><b>Video ID</b></td>
+                            @endif --}}
 
-                            <td align="left"><b>Contact No.</b></td>
-                            <td align="left"><b>Email ID</b></td>
-                            <td align="left"><b>Confirmed</b></td>
-                            <td align="left"><b>Source</b></td>
+                            @if(isset($skype_id) && $skype_id != '')
+                                <td align="center"><b>Video ID</b></td>
+                            @endif 
+
+                            <td align="center"><b>Contact No.</b></td>
+                            <td align="center"><b>Email ID</b></td>
+                            <td align="center"><b>Confirmed</b></td>
+                            <td align="center"><b>Source</b></td>
                         </tr>
                         <?php $i=0; ?>
                         <tr style="font-family:Cambria, serif;font-size: 11.0pt;">
-                            <td align="left">{{ ++$i }}</td>
-                            <td align="left">{{$job_designation}}</td>
-                            <td align="left">{{$city}}</td>
-                            <td align="left">{{$cname}}</td>
-                            <td align="left">{{date('d/m/Y',strtotime($interview_date)) }}</td>
-                            <td align="left">{{date('h:i A',strtotime($interview_time))  }}</td>
+                            <td align="center">{{ ++$i }}</td>
+                            <td align="center">{{$job_designation}}</td>
+                            <td align="center">{{$city}}</td>
+                            <td align="center">{{$cname}}</td>
+                            <td align="center">{{date('d/m/Y',strtotime($interview_date)) }}</td>
+                            <td align="center">{{date('h:i A',strtotime($interview_time))  }}</td>
 
                             @if(isset($interview_type) && $interview_type == 'Personal Interview')
-                                <td align="left">{{$interview_location}}</td>
+                                <td align="center">{{$interview_location}}</td>
                             @endif
 
-                            <td align="left">{{$candidate_location}}</td>
-                            <td align="left">{{$interview_type}}</td>
+                            <td align="center">{{$candidate_location}}</td>
+                            <td align="center">{{$interview_type}}</td>
                             
-                            @if((isset($interview_type) && $interview_type == 'Personal Interview') || (isset($interview_type) && $interview_type == 'Telephonic Interview'))
+                            {{-- @if((isset($interview_type) && $interview_type == 'Personal Interview') || (isset($interview_type) && $interview_type == 'Telephonic Interview'))
 
                             @else
-                                <td align="left">{{$skype_id}}</td>
+                                <td align="center">{{$skype_id}}</td>
+                            @endif --}}
+
+                            @if(isset($skype_id) && $skype_id != '')
+                                <td align="center">{{ $skype_id }}</td>
                             @endif 
 
-                            <td align="left">{{$cmobile}}</td>
-                            <td align="left">{{$cemail}}</td>
-                            <td align="left"><b>{{'Yes'}}</b></td>
-                            <td align="left"><b>{{'Adler'}}</b></td>
+                            <td align="center">{{$cmobile}}</td>
+                            <td align="center">{{$cemail}}</td>
+                            <td align="center"><b>{{'Yes'}}</b></td>
+                            <td align="center"><b>{{'Adler'}}</b></td>
                         </tr>
                     </table>
                 <p style="font-family:Cambria, serif;font-size: 11.0pt;text-align: left;">Hope this works.</p>
