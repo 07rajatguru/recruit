c8f80f7c5e46b9aa09ae08a5b09b4857215a8690
Dhara Khandhediya <dhara@trajinfotech.com>
2021-12-22 13:40:04 +0530

    add department id when lead convert to client

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -916,6 +916,18 @@ class LeadController extends Controller
         $client_basic_info->updated_at = time();
         $client_basic_info->delete_client = 0;
 
+        // Save Department Id for Different Dashboard
+        $vibhuti_user_id = getenv('STRATEGYUSERID');
+
+        if($user_id == $vibhuti_user_id) {
+
+            $client_basic_info->department_id = 2;
+        }
+        else {
+
+            $client_basic_info->department_id = 1;
+        }
+
         if($client_basic_info->save()) {
 
             // If client basic info added successfully then lead convert to client
