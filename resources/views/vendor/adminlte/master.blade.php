634846ddbc96099f9871225521b2ab57c190aa22
Dhara Khandhediya <dhara@trajinfotech.com>
2022-03-12 22:38:06 +0530

    remove cdn & download in local

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -22,22 +22,28 @@
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/custom.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/bootstrap-datetimepicker.css') }}"><!-- 
+
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.44/css/bootstrap-datetimepicker.css" />
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.44/css/bootstrap-datetimepicker.css" /> -->
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/select2.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/bootstrap3-wysihtml5.min.css') }}">
 
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+    <!-- <link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.1/css/responsive.dataTables.min.css">
-    <link href="https://cdn.datatables.net/fixedheader/3.1.2/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/fixedheader/3.1.2/css/fixedHeader.bootstrap.min.css" rel="stylesheet"> -->
 
-    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.print.css">--}}
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/jquery.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/responsive.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/fixedHeader.bootstrap.min.css') }}">
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/> -->
+
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/fullcalendar.min.css') }}">
 
     <link href="/css/jquery.fancybox.css" rel="stylesheet">
 
@@ -47,10 +53,7 @@
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-
-            <![endif]-->
-
-
+    <![endif]-->
 
 </head>
 <body class="hold-transition @yield('body_class')">
@@ -61,20 +64,34 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 {{--<script src="{{ asset('vendor/adminlte/bootstrap/js/bootstrap.min.js') }}"></script>--}}
 {{--<script src="{{ asset('vendor/adminlte/js/bootstrap.min.js') }}"></script>--}}
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+<script src="{{ asset('vendor/adminlte/js/bootstrap.min.js') }}"></script>
+
 <script src="{{ asset('vendor/adminlte/js/moment.js') }}"></script>
 <script src="{{ asset('vendor/adminlte/js/select2.min.js') }}"></script>
+
 <!-- Bootstrap WYSIHTML5 -->
 <script src="{{ asset('vendor/adminlte/js/bootstrap3-wysihtml5.all.min.js') }}"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.44/js/bootstrap-datetimepicker.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.44/js/bootstrap-datetimepicker.min.js"></script>-->
 
-<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('vendor/adminlte/js/bootstrap-datepicker.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/js/bootstrap-datetimepicker.min.js') }}"></script>
+
+<!-- <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/fixedheader/3.1.2/js/dataTables.fixedHeader.min.js"></script>
 <script src="https://cdn.datatables.net/fixedcolumns/3.2.4/js/dataTables.fixedColumns.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script> -->
+
+<script src="{{ asset('vendor/adminlte/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/js/dataTables.fixedHeader.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/js/dataTables.fixedColumns.min.js') }}"></script>
+<script src="{{ asset('vendor/adminlte/js/fullcalendar.min.js') }}"></script>
+
 <script src="/js/jquery.fancybox.js"></script>
 @yield('adminlte_js')
 @yield('customscripts')
