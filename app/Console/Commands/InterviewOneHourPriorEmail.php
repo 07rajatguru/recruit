a471e139c8909352bf032ebff8fb24bcdebea123
Meet Jani <meetjani26>
2022-09-27 23:40:55 +0530

    Interview Reminder mail send as one hour block

--- app/Console/Commands/InterviewOneHourPriorEmail.php
+++ app/Console/Commands/InterviewOneHourPriorEmail.php
@@ -43,6 +43,7 @@ class InterviewOneHourPriorEmail extends Command
         $from_date = date("Y-m-d 00:00:00");
         $to_date = date("Y-m-d 23:59:59");
         $curr_date_time = date('Y-m-d H:i:00', time() + 19800);
+        $one_hour_back_time = date('Y-m-d H:i:00', strtotime($curr_date_time. "-1 hour"));
 
         $recruitment = getenv('RECRUITMENT');
         $hr_advisory = getenv('HRADVISORY');
@@ -50,29 +51,21 @@ class InterviewOneHourPriorEmail extends Command
         $type_array = array($recruitment,$hr_advisory,$management);
 
         $users = User::getAllUsers($type_array);
-
         if(isset($users) && sizeof($users) > 0) {
-
             foreach ($users as $key => $value) {
-                
                 $interviews = Interview::getAllInterviewsByReminders($key,$from_date,$to_date);
-
                 if(isset($interviews) && sizeof($interviews) > 0) {
-
                     $to_address = array();
                     $module_ids_array = array();
                     $j = 0;
-
                     foreach ($interviews as $key1 => $value1) {
-
                         if(isset($value1) && $value1 != '') {
-
                             $get_interview_date = $value1['interview_date_actual'];
 
-                            $one_hour_ago_interview_date = date("Y-m-d H:i:00",strtotime($get_interview_date . " - 1 hour"));
+                            // $one_hour_ago_interview_date = date("Y-m-d H:i:00",strtotime($get_interview_date . " - 1 hour"));
                                
-                            if($one_hour_ago_interview_date == $curr_date_time) {
-
+                            // if($one_hour_ago_interview_date == $curr_date_time) {
+                            if($get_interview_date >= $one_hour_back_time && $get_interview_date <= $curr_date_time) {
                                 $module_ids_array[$j] = $value1['id'];
                                 $j++;
                             }
@@ -80,7 +73,6 @@ class InterviewOneHourPriorEmail extends Command
                     }
 
                     if(isset($module_ids_array) && sizeof($module_ids_array) > 0) {
-                        
                         $module = "Interview Reminder";
                         $sender_name = $key;
                         $to = User::getUserEmailById($key);
