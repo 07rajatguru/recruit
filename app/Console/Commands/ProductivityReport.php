c7a7ccdaa8a274632865ddd89006feb5f2d9f016
Meet Jani <meetjani26>
2022-11-08 22:43:17 +0530

    Productivity report > 3rd saturday off

--- app/Console/Commands/ProductivityReport.php
+++ app/Console/Commands/ProductivityReport.php
@@ -42,58 +42,76 @@ class ProductivityReport extends Command
      */
     public function handle() {
 
-        $recruitment = getenv('RECRUITMENT');
-        $users = User::getAllUsersEmails($recruitment,'Yes','',0);
+        $date = date('Y-m-d');
 
-        $superadminuserid = getenv('SUPERADMINUSERID');
-        $superadminemail = User::getUserEmailById($superadminuserid);
+        // Get All Saturday dates of current month
+        $s_date = date("Y-m-d", strtotime("first day of this month"));
+        $first_day = date('N',strtotime($s_date));
+        $first_day = 6 - $first_day + 1;
+        $last_day =  date('t',strtotime($s_date));
+        $saturdays = array();
+        for($i = $first_day; $i <= $last_day; $i = $i+7 ) {
+            $saturdays[] = $i;
+        }
 
-        $hr = getenv('HRUSERID');
-        $hremail = User::getUserEmailById($hr);
+        // Get Saturday Date
+        $saturday_date = date('Y-m')."-".$saturdays[2];
+        if ($saturday_date == $date) {
+        }
+        else {
+            $recruitment = getenv('RECRUITMENT');
+            $users = User::getAllUsersEmails($recruitment,'Yes','',0);
 
-        $manager_user_id = getenv('MANAGERUSERID');
-        $manager_email = User::getUserEmailById($manager_user_id);
+            $superadminuserid = getenv('SUPERADMINUSERID');
+            $superadminemail = User::getUserEmailById($superadminuserid);
 
-        foreach ($users as $key => $value) {
+            $hr = getenv('HRUSERID');
+            $hremail = User::getUserEmailById($hr);
 
-            $check_users_log_count = UsersLog::getUserLogsOfWeekById($key);
+            $manager_user_id = getenv('MANAGERUSERID');
+            $manager_email = User::getUserEmailById($manager_user_id);
 
-            if(isset($check_users_log_count) && $check_users_log_count > 0) {
+            foreach ($users as $key => $value) {
 
-                $user_benchmark = UserBenchMark::getBenchMarkByUserID($key);
+                $check_users_log_count = UsersLog::getUserLogsOfWeekById($key);
 
-                if(isset($user_benchmark) && sizeof($user_benchmark) > 0) {
+                if(isset($check_users_log_count) && $check_users_log_count > 0) {
 
-                    $cc_array = array();
-                    $res = User::getReportsToUsersEmail($key);
+                    $user_benchmark = UserBenchMark::getBenchMarkByUserID($key);
 
-                    if(isset($res->remail) && $res->remail!='') {
+                    if(isset($user_benchmark) && sizeof($user_benchmark) > 0) {
 
-                        $report_email = $res->remail;
-                        $cc_array = array($report_email,$manager_email,$superadminemail,$hremail);
+                        $cc_array = array();
+                        $res = User::getReportsToUsersEmail($key);
 
+                        if(isset($res->remail) && $res->remail!='') {
+
+                            $report_email = $res->remail;
+                            $cc_array = array($report_email,$manager_email,$superadminemail,$hremail);
+
+                        }
+                        else {
+                            $cc_array = array($manager_email,$superadminemail,$hremail);
+                        }
+                        
+                        $to_array = array();
+                        $to_array[] = $value;
+
+                        $user_name = User::getUserNameById($key);
+
+                        $module = "Productivity Report";
+                        $subject = 'Productivity Report - ' . $user_name;
+                        $message = "";
+                        $to_array = array_filter($to_array);
+                        $to = implode(",",$to_array);
+
+                        $cc_array = array_filter($cc_array);
+                        $cc = implode(",",$cc_array);
+                        $module_id = 0;
+                        $sender_name = $key;
+
+                        event(new NotificationMail($module,$sender_name,$to,$subject,$message,$module_id,$cc));
                     }
-                    else {
-                        $cc_array = array($manager_email,$superadminemail,$hremail);
-                    }
-                    
-                    $to_array = array();
-                    $to_array[] = $value;
-
-                    $user_name = User::getUserNameById($key);
-
-                    $module = "Productivity Report";
-                    $subject = 'Productivity Report - ' . $user_name;
-                    $message = "";
-                    $to_array = array_filter($to_array);
-                    $to = implode(",",$to_array);
-
-                    $cc_array = array_filter($cc_array);
-                    $cc = implode(",",$cc_array);
-                    $module_id = 0;
-                    $sender_name = $key;
-
-                    event(new NotificationMail($module,$sender_name,$to,$subject,$message,$module_id,$cc));
                 }
             }
         }
